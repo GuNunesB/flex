03d1e18d16ad3ba6d60a4a7f9f2e465862763315
GuNunesB <gununes280@gmail.com>
2025-02-06 22:20:43 -0300

    app

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Calculadora flex
+ * @author Gustavo Nunes Bispo
+ * @version 2.0
+ */
+
+function calcular() {
+    //capturar os valores das caixas de input
+    let gasolina = document.getElementById('gasolina').value
+    let etanol = document.getElementById('etanol').value
+    let kmlGasolina = document.getElementById('kmGasolina').value
+    let kmlEtanol = document.getElementById('kmEtanol').value
+
+    /*
+    console.log(gasolina)
+    console.log(etanol)
+    console.log(kmlGasolina)
+    console.log(kmlEtanol)
+    */
+
+    if (etanol< (kmlEtanol / kmlGasolina) * gasolina) {
+        document.getElementById('status').src = "img/etanol.png"
+    } else {
+        document.getElementById('status').src = "img/gasolina.png"
+    }
+}
